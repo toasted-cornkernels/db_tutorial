218c92c6a93a88ee659ee56f5b05712980f09cd0
toasted-cornkernels <volcano0909@gmail.com>
2023-12-13 22:13:58 -0800

    finish Part2

--- Port/OCamlPort/lib/Part2.ml
+++ Port/OCamlPort/lib/Part2.ml
@@ -4,10 +4,6 @@ module MetaCommand = struct
   type t = Success | Unrecognized [@@deriving compare, equal]
 end
 
-module PrepareResult = struct
-  type t = Success of Statement.t | Unrecognized [@@deriving compare, equal]
-end
-
 module Statement = struct
   module Type = struct
     type t = Insert | Select [@@deriving compare, equal]
@@ -16,11 +12,15 @@ module Statement = struct
   type t = Type of Type.t [@@deriving compare, equal]
 end
 
+module PrepareResult = struct
+  type t = Success of Statement.t | Unrecognized [@@deriving compare, equal]
+end
+
 let exit_metacommand = ".exit"
 
-let prepare_statement (input_command : String.t) : Statement.t =
-  if String.startswith command ~prefix:"insert" then Success (Type Insert)
-  else if String.startswith command ~prefix:"select" then Success (Type Select)
+let prepare_statement (input_command : String.t) : PrepareResult.t =
+  if String.is_prefix input_command ~prefix:"insert" then Success (Type Insert)
+  else if String.is_prefix input_command ~prefix:"select" then Success (Type Select)
   else Unrecognized
 
 
@@ -28,10 +28,12 @@ let execute_statement (statement : Statement.t) =
   match statement with Type Insert -> `STUB | Type Select -> `STUB
 
 
-let do_meta_command = raise TODO
+let execute_meta_command (input_command : String.t) : MetaCommand.t =
+  if String.equal input_command exit_metacommand then exit 0 else Unrecognized
+
 
 let rec part2_main () =
-  let input_command : String.t = Scanf.scanf "db> %s" Fn.identity in
+  let input_command : String.t = Scanf.scanf "db> %s" Fn.id in
   if String.equal input_command exit_metacommand then ()
   else Printf.printf "Unrecognized command '%s'.\n" input_command ;
   part2_main ()
